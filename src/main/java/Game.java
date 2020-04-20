7c7e5d1087a04c7be67ba8f440662537dd8c046e
kyrafenech <kyra.fenech.18@um.edu.mt>
2020-04-20 17:00:10 +0200

    Setting map size according to number of players

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.File;
+
 public class Game {
+
+    private int turns;
+    private Player[] players;
+    private Map map;
+
+
+    public boolean setNumPlayers(int playerCount) {
+        int minPlayers = 2;
+        int maxPlayers = 8;
+
+        //validating given number of players [2 < n < 8]
+        if (playerCount < minPlayers || playerCount > maxPlayers) {
+            System.out.println("Only between 2 and 8 players are accepted.");
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean setMapSize(int size, int playercount) {
+        final int MAX = 50;
+        //setting minimum number of map size according to amount of players
+        final int MIN = playercount <= 4 ? 5 : 8;
+
+        //validating given size
+        if (MIN >= size) {
+            System.out.println("Given map size is too small.");
+            return false;
+        } else if (MAX <= size) {
+            System.out.println("Given map size is too big.");
+            return false;
+        }
+        return true;
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -2,5 +2,59 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class GameTest {
+
+    Game game;
+    Player player1;
+    Player player2;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        game = null;
+    }
+
+    @Test
+    public void setNumPlayers_LessThan2() {
+        boolean result = game.setNumPlayers(1);
+        assertFalse(result);
+    }
+
+    @Test
+    public void setNumPlayers_MoreThan8() {
+        boolean result = game.setNumPlayers(10);
+        assertFalse(result);
+    }
+    @Test
+    public void setNumPlayers() {
+        boolean result = game.setNumPlayers(5);
+        assertTrue(result);
+    }
+    @Test
+    public void setMapSize_TooSmall() {
+        boolean result = game.setMapSize(4,4);
+        assertFalse(result);
+    }
+
+    @Test
+    public void setMapSize_TooBig() {
+        boolean result = game.setMapSize(60,4);
+        assertFalse(result);
+    }
+
+    @Test
+    public void setMapSize() {
+        boolean result = game.setMapSize(30,6);
+        assertTrue(result);
+    }
+
+
+
 }

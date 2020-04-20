d20d1668c5271aa0d5dbc01f33a5c25030d8fb1c
mariahzm <mariah.zammit.18@um.edu.mt>
2020-04-20 17:10:17 +0200

    basic structure for Map class

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,7 +1,21 @@
+import java.util.Random;
+
 public class Map {
 
-    public Map(){
+    private int size;
+    private Tile[][] grid;
+
+    //class constructor
+    public Map(int size){
+        this.size = size;
+        this.grid = new Tile[size][size];
+        generate();
+    }
+
+    //filling the grid elements with tile types
+    private void generate(){
 
     }
+
 
 }

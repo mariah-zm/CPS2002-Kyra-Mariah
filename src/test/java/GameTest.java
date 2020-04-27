13f1a74497f8527ebdc3fe70e832d2febfffa983
mariahzm <mariah.zammit.18@um.edu.mt>
2020-04-27 18:37:10 +0200

    solved all build issues and conflict problem

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,23 +1,30 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 
 import static org.junit.Assert.*;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
 
 public class GameTest {
 
     Game game;
-    Map map;
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    @Rule
+    public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp(){
         game = new Game();
-        map = new Map(25);
-
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown(){
         game = null;
     }
 
@@ -39,23 +46,39 @@ public class GameTest {
     }
     @Test
     public void setMapSize_TooSmall() {
-        game.setNumPlayers(4);
-        boolean result = game.setMapSize(4);
+        boolean result = game.setMapSize(4,4);
         assertFalse(result);
     }
 
     @Test
     public void setMapSize_TooBig() {
-        game.setNumPlayers(8);
-        boolean result = game.setMapSize(60);
+        boolean result = game.setMapSize(60,4);
         assertFalse(result);
     }
 
     @Test
     public void setMapSize() {
-        game.setNumPlayers(6);
-        boolean result = game.setMapSize(30);
+        boolean result = game.setMapSize(30,6);
         assertTrue(result);
     }
 
+    @Test
+    public void main_InputsFoundAndCorrect(){
+        systemInMock.provideLines("2", "5");
+        exit.expectSystemExitWithStatus(0);
+        game.main(null);
+    }
+
+    @Test
+    public void main_InputsNotImmediatelyCorrect(){
+        systemInMock.provideLines("incorrect", "2", "incorrect", "5");
+        exit.expectSystemExitWithStatus(0);
+        game.main(null);
+    }
+
+    @Test
+    public void main_noInputFound(){
+        exit.expectSystemExitWithStatus(1);
+        game.main(null);
+    }
 }
